5fb2999bb7a74e158023a229c4dedd35636ad24a
YujiaLi1994 <yul178@pitt.edu>
2022-03-17 21:23:58 -0400

    111

---
+++ ogClust.WJL/R/Sim_continuous.R
@@ -0,0 +1,70 @@
+#' @title Simulation of continuous outcome in the paper
+#' @param n number of total samples (the first n/3 samples belong to cluster 1, next n/3 belongs to cluster 2 and the last n/3 belong to cluster 3)
+#' @param beta1 coefficent of first covariate
+#' @param beta2 coefficent of second covariate
+#' @param q number of true informative genes
+#' @param q1 number of genes for random clusters
+#' @param q2 number of genes correlated with covariates
+#' @param q3 number of noise genes
+#' @param c1 effect size of outcome separation
+#' @param var_g control the correlation of the q3 genes with covariates
+#' @param mu effect size of gene separation
+#' @param mu1 effect size of random clusters
+#' @param sigma_y standard deviation for outcome
+#'
+#' @return
+#' result_list: A list with three components.
+#' \itemize{
+#' \item{x: }{simulated covariate matrix}
+#' \item{y: }{simulated outcome}
+#' \item{G: }{simulated gene expression data}
+#' }
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' Data<-Sim_surv(n=99,beta1 = 0.5,beta2 = 0.5,q = 50,q1 = 300,q2 = 50,q3 = 1600,c1 = 2,var_g = 2,mu = 1.5,mu1 = 1.7,sigma_y = 0.5,censor = 100)
+#' Data<-Sim_continuous(n=99,beta1 = 1,beta2 = 1,q = 50,q1 = 50,q2 = 50,q3 = 1850,c1 = 3,var_g = 2,mu = 1.5,mu1 = 1.7,sigma_y = 1)
+#' G<-Data$G
+#' X<-Data$x
+#' Y<-Data$y
+#' }
+
+
+Sim_continuous<-function(n,beta1,beta2,q,q1,q2,q3,c1,var_g,mu,mu1,sigma_y){
+  #simulate covariates from N(0,1)
+  x1<-rnorm(n,0,1)
+  x2<-rnorm(n,0,1)
+  x<-cbind(x1,x2)
+  #simulate y
+  y<-rep(NA,n)
+  y[1:(n/3)]<-rnorm(n/3,0+beta1*x1[1:(n/3)]+beta2*x2[1:(n/3)],sigma_y)
+  y[(n/3+1):(2*n/3)]<-rnorm(n/3,c1+beta1*x1[(n/3+1):(2*n/3)]+beta2*x2[(n/3+1):(2*n/3)],sigma_y)
+  y[(2*n/3+1):n]<-rnorm(n/3,2*c1+beta1*x1[(2*n/3+1):n]+beta2*x2[(2*n/3+1):n],sigma_y)
+  #simulate x
+  X1 = rbind(matrix(rnorm((n/3) * q), ncol = q),
+             matrix(rnorm((n/3) * q), ncol = q),
+             matrix(rnorm((n/3) * q), ncol = q))
+  X2 = rbind(matrix(rnorm((n/3) * q1), ncol = q1),
+             matrix(rnorm((n/3) * q1), ncol = q1),
+             matrix(rnorm((n/3) * q1), ncol = q1))
+  X3 = rbind(matrix(rnorm((n/3) * q2), ncol = q2),
+             matrix(rnorm((n/3) * q2), ncol = q2),
+             matrix(rnorm((n/3) * q2), ncol = q2))
+  X.noise = rbind(matrix(rnorm((n/3) * q3), ncol = q3),
+                  matrix(rnorm((n/3) * q3), ncol = q3),
+                  matrix(rnorm((n/3) * q3), ncol = q3))
+  X1[1:(n/3), 1:q] <- X1[1:(n/3), 1:q] - mu
+  X1[(n/3+1):(2*n/3), 1:q] <- X1[(n/3+1):(2*n/3), 1:q]
+  X1[(2*n/3+1):n, 1:q] <- X1[(2*n/3+1):n, 1:q] + mu
+  index<-sample(1:n)
+  X2[index[1:(n/3)], ] <- X2[index[1:(n/3)],]-mu1
+  X2[index[(n/3+1):(2*n/3)], ] <- X2[index[(n/3+1):(2*n/3)], ]
+  X2[index[(2*n/3+1):n], ] <- X2[index[(2*n/3+1):n], ]+mu1
+  for(i in 1:ncol(X3)){
+    X3[,i]<-X3[,i]+rnorm(1,0,var_g)*x1
+  }
+  X<-cbind(X1,X2,X3,X.noise)
+  data<-list(x=x,y=y,G=X)
+  return(data)
+}
---
+++ ogClust.WJL/R/region_lambda_surv.R
@@ -0,0 +1,69 @@
+#' @title efficient lambda grids for a given K, for survival outcome
+#' @param lambda1 a large lambda which gives close to 0 informative genes
+#' @param lambda2 a small lambda which includes almost all the genes (default is lambda2=0)
+#' @param iteration Number of iterations
+#' @param Y outcome
+#' @param G gene expression matrix (row is samples, column is genes)
+#' @param X covariates matrix
+#' @param center initial center for ogClust
+#' @param w weight of outcome
+#' @param K Number of clusters
+#' @param delta censor indicator of survival outcome
+#' @return a list with two components
+#' \itemize{
+#' \item{lambda: }{generated lambda grids}
+#' \item{num: }{number of informative genes corresponding to each lambda}
+#' }
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' Data<-Sim_surv(n=99,beta1 = 0.5,beta2 = 0.5,q = 50,q1 = 300,q2 = 50,q3 = 1600,c1 = 2,var_g = 2,mu = 1.5,mu1 = 1.7,sigma_y = 0.5,censor = 100)
+#' G<-Data$G
+#' X<-Data$x
+#' Y<-Data$y
+#' Y.ind<-Data$y.ind
+#' mod.kmeans<-kmeans(G,centers = 3,nstart = 20)
+#' center<-t(mod.kmeans$centers)
+#' region_lambda_surv(lambda1=15,lambda2 = 0,iteration = 10,Y = Y,G = G,X = X,center =center ,w = 0.5,K = 3,delta = Y.ind)
+#' }
+
+region_lambda_surv<-function (lambda1 =18,lambda2=0,iteration = 10, Y, G,X,center,w,K=K,delta){
+  lambda.vector <- c(lambda1, lambda2)
+  num.vector <- rep(-1, length(lambda.vector))
+  for (i in 1:length(num.vector)) {
+    mod1 <- ogClust_Surv(x=X,G=t(G),y=Y,y.ind = delta,c_center=center,lambda=lambda.vector[i],v_int=NULL,pi_int=NULL,K=K,max_iter=200,w_outcome=w,w_G=1-w,z_int=NULL)
+    num.vector[i] <- sum(apply(mod1$result_list$mu,1,function(x){length(unique(x))})!=1)
+  }
+  num.vector_old <- num.vector
+  lambda <- (lambda.vector[1]+lambda.vector[2])/2
+  iter<-2
+  while (iter <= iteration) {
+    #print(iter)
+    lambda.vector <- sort(c(lambda.vector, lambda),decreasing = T)
+    num.vector <- rep(-1, length(lambda.vector))
+    num.vector[-which(lambda.vector == lambda)] <- num.vector_old
+    mod_new<-ogClust_Surv(x=X,G=t(G),y=Y,y.ind = delta,c_center=center,lambda=lambda,v_int=NULL,pi_int=NULL,K=K,max_iter=200,w_outcome=w,w_G=1-w,z_int=NULL)
+
+    num.vector[which(lambda.vector == lambda)] <- sum(apply(mod_new$result_list$mu,1,function(x){length(unique(x))})!=1)
+    d <- rep(-1, (length(lambda.vector) - 1))
+    for (i in 1:length(d)) {
+      if (num.vector[i] == num.vector[i + 1]) {
+        d[i] <- 0
+      }else {
+        d[i] <- abs(log2(num.vector[i + 1]/num.vector[i]))
+      }
+    }
+    index <- which.max(d)
+    lambda <- (lambda.vector[index]+lambda.vector[index + 1])/2
+    iter <- iter + 1
+    num.vector_old <- num.vector
+  }
+  lambda.vector <- sort(c(lambda.vector, lambda),decreasing =T)
+  num.vector <- rep(-1, length(lambda.vector))
+  num.vector[-which(lambda.vector == lambda)] <- num.vector_old
+  mod_new<-ogClust_Surv(x=X,G=t(G),y=Y,y.ind = delta,c_center=center,lambda=lambda,v_int=NULL,pi_int=NULL,K=K,max_iter=200,w_outcome=w,w_G=1-w,z_int=NULL)
+  num.vector[which(lambda.vector == lambda)] <- sum(apply(mod_new$result_list$mu,1,function(x){length(unique(x))})!=1)
+  res<-list(lambda=lambda.vector,num=num.vector)
+  return(res)
+}
